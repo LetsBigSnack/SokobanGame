6e9ab2820ee4cebabc856f0cc19957b8587af3c4
elina <117643112+thisis-elina@users.noreply.github.com>
2024-04-12 17:55:40 +0200

    Transformed Player-Class to Singelton

--- game/obj/Debug/net8.0/game.AssemblyInfo.cs
+++ game/obj/Debug/net8.0/game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f57b44f2bba619c75b7e5e5b5ea1968e8f620b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f71693b6af7072605b403291db39ec083dc6bae")]
 [assembly: System.Reflection.AssemblyProductAttribute("game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
