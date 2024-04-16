39cc4fe83381a69357c55d73b3141f0dff3761f4
LetsBigSnack <83255386+LetsBigSnack@users.noreply.github.com>
2024-04-16 21:46:17 +0200

    Merge Conflict Resolved

--- game/obj/Debug/net8.0/game.AssemblyInfo.cs
+++ game/obj/Debug/net8.0/game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f71693b6af7072605b403291db39ec083dc6bae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3bb4e6a499a77b0b50ad4a460b223a1c2a0d2b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
